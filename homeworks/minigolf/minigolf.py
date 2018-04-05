7df1fa6de7ef66289569c005d2fc1d1bd59579e3
arteshokV <vlad_zahar@mail.ru>
2018-04-05 16:20:44 +0300

    HW3 done second match rules

--- homeworks/minigolf/minigolf.py
+++ homeworks/minigolf/minigolf.py
@@ -26,7 +26,21 @@ class Match(metaclass=ABCMeta):
         if not self._finished:
             raise RuntimeError('Not finished')
         else:
-            return self.select_winners()
+            points_array = []
+            return_array = []
+            for playernum in range(self._number_of_players):
+                sum = 0
+                for holenum in range(self._number_of_holes):
+                    sum += self._results_table[holenum][playernum]
+                points_array.append(sum)
+            if type(self) == HitsMatch:
+                winner_points = min(points_array)
+            else:
+                winner_points = max(points_array)
+            for idn, points in enumerate(points_array):
+                if (winner_points == points):
+                    return_array.append(self._players_array[idn])
+            return return_array
 
     def get_table(self):
         return_array = []
@@ -46,32 +60,40 @@ class Match(metaclass=ABCMeta):
                 return_array.append(tuple(self._results_table[hole]))
         return return_array
 
+    def _switch_to_next_hole(self):
+        # going to the next hole
+        for player_object in self._players_array:
+            player_object.hits_no_more = 0
+        self._current_hole += 1
+        if self._current_hole == self._number_of_holes:
+            self._finished = True
+        self._current_player = self._current_hole
+
+    def _everybody_finished_hole(self):
+        for player in self._players_array:
+            if player.hits_no_more == 0:
+                return False
+        return True
+
+    def _set_next_player_and_hole_if_needed(self):
+        self._current_player = self._current_player + 1 if self._current_player != self._number_of_players-1 else 0
+
+        if self._everybody_finished_hole():
+            self._switch_to_next_hole()
+            return 1
+
+        while self._players_array[self._current_player].hits_no_more == 1:
+            self._current_player = self._current_player + 1 if self._current_player != self._number_of_players-1 else 0
+        return 0
+
     @abstractmethod
     def hit(self, success=False):
         pass
 
-    @abstractmethod
-    def select_winners(self):
-        pass
 
 
 
 class HitsMatch(Match):
-    def _set_next_player_and_hole_if_needed(self):
-        self._current_player = self._current_player + 1 if self._current_player != self._number_of_players-1 else 0
-        number_of_loops = 0
-        while self._players_array[self._current_player].hits_no_more == 1:
-            self._current_player = self._current_player + 1 if self._current_player != self._number_of_players-1 else 0
-            number_of_loops += 1
-            if number_of_loops > self._number_of_players:
-                #going to the next hole
-                for player_object in self._players_array:
-                    player_object.hits_no_more = 0
-                self._current_hole += 1
-                if self._current_hole == self._number_of_holes:
-                    self._finished = True
-                self._current_player = self._current_hole
-                break
 
     def hit(self, success=False):
         if self._finished == True:
@@ -87,36 +109,34 @@ class HitsMatch(Match):
             self._players_array[self._current_player].hits_no_more = 1
 
         self._set_next_player_and_hole_if_needed()
-
-    def select_winners(self):
-        points_array = []
-        return_array = []
-        for playernum in range(self._number_of_players):
-            sum = 0
-            for holenum in range(self._number_of_holes):
-                sum += self._results_table[holenum][playernum]
-            points_array.append(sum)
-        minpoint = min(points_array)
-        for idn,points in enumerate(points_array) :
-            if(minpoint == points):
-                return_array.append(self._players_array[idn])
-        return return_array
 
 
 class HolesMatch(Match):
+    def __init__(self, number_of_holes, palyers_array):
+        Match.__init__(self, number_of_holes, palyers_array)
+        self._number_of_fails = 0
+        self._was_goal = False
+
     def hit(self, success=False):
         if self._finished == True:
             raise RuntimeError('Already finished')
+
         current_hole_array = self._results_table[self._current_hole]
-        current_hole_array[self._current_player] = current_hole_array[self._current_player] + 1 if \
-            current_hole_array[self._current_player] else 1
-
         if success:
+            current_hole_array[self._current_player] = 1
             self._players_array[self._current_player].hits_no_more = 1
-        if self._results_table[self._current_hole][self._current_player] == 9:
-            current_hole_array[self._current_player] += 1
-            self._players_array[self._current_player].hits_no_more = 1
+            self._was_goal = True
+        else:
+            self._number_of_fails += 1
+            if self._was_goal == True:
+                self._players_array[self._current_player].hits_no_more = 1
+                current_hole_array[self._current_player] = 0
 
-        self._set_next_player_and_hole_if_needed()
-    def select_winners(self):
-        pass
+        if self._number_of_fails == self._number_of_players*10:
+            for playerind, player in enumerate(self._players_array):
+                player.hits_no_more = 1
+                current_hole_array[playerind] = 0
+
+        if self._set_next_player_and_hole_if_needed():
+            self._was_goal = False
+            self._number_of_fails = 0
